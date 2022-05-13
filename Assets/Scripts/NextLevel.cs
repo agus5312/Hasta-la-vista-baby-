47eca37ba68f45fed541053e305d8aa0aa072ff5
Basilio Sainz <BasilioSainz@outlook.com>
2022-05-13 05:16:28 -0300

    Musica y detalles Buenas noches uwu

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -5,23 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    public float timeToChangeLevel;
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            Debug.Log("Tocó el goal");
-            StartCoroutine("ChangeToNextLevel");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
-
-    IEnumerator ChangeToNextLevel()
-    {
-        yield return new WaitForSeconds(timeToChangeLevel);
-        SceneManager.LoadScene("lvl select");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("Next Level");
-    }
 }
