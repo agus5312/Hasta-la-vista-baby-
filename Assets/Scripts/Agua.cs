d2835a659dafe458606685962a41da8ea659a5e7
Basilio Sainz <BasilioSainz@outlook.com>
2022-05-13 00:36:52 -0300

    SFX Trapo e implementacion

--- Assets/Scripts/Agua.cs
+++ Assets/Scripts/Agua.cs
@@ -6,14 +6,19 @@ public class Agua : MonoBehaviour
 {
     int limpieza = 5;
     SpriteRenderer sprite;
+
+    public AudioSource soundChanel;
+    public AudioClip trapo;
     private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
+        soundChanel.clip = trapo;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Trapo")
         {
+            soundChanel.Play();
             limpieza -= 1;
             sprite.color = new Color(1f, 1f, 1f, sprite.color.a - 0.2f);
             if (limpieza <= 0)
